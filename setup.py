9d46fa231d0b9299b8ae63e166edb29225368f94
jawad-r <jawadaliraja2022@gmail.com>
2026-02-19 00:05:16 +0500

    Set up structure of the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jawad Ali",
+    author_email="jawadaliraja2024@gmail.com",
+    packages=find_packages()
+)
